12eac7eecc5d1ece1ad7c8668f588371f1d2cf55
Cody Vansickle <36089471+CVansickle1@users.noreply.github.com>
2022-08-11 14:18:52 -0400

    Update Program.cs
    Edited wording

--- SyntaxExercise/Program.cs
+++ SyntaxExercise/Program.cs
@@ -8,7 +8,7 @@ namespace SyntaxExercise
         {
             Console.WriteLine("Hello World!");
             var answer = 4;
-            var response = (answer <= 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
             Console.WriteLine(response);
             
         }
